4fce7e5f66876ae9d876008de42fabe6701a8ed9
Immanuel Sun <immanuelsun@newstreamsdesign.com>
2014-07-12 02:26:04 -0700

    fixed bugs: font and other few items

--- index.php
+++ index.php
@@ -2,51 +2,52 @@
 <!-- [if IE 8]> <html lang="en" class="ie8"> <![endif]-->
 <!-- [if !IE]> <!--> <html lang="en"> <!--<![endif]-->
 
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
-    <meta name="desciption"  content="">
-    <meta name="author"  content="">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title></title>
+  <meta name="desciption"  content="">
+  <meta name="author"  content="">
 
-    <!-- Mobile Specific Meta -->
-   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, use-scalable=no">
+  <!-- Mobile Specific Meta -->
+ <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
-    <!-- Bootstrap -->
-    <!-- <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">-->
+  <!-- Bootstrap -->
+  <!-- <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">-->
 
-    <!-- Bootstrap: Local -->
-    <link rel="stylesheet" href="css/style.css">
+  <!-- Bootstrap: Local -->
+  <link rel="stylesheet" href="css/style.css">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="../../docs-assets/js/html5shiv.js"></script>
-      <script src="../../docs-assets/js/respond.min.js"></script>
-    <![endif]-->
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+    <script src="../../docs-assets/js/html5shiv.js"></script>
+    <script src="../../docs-assets/js/respond.min.js"></script>
+  <![endif]-->
 
-    <!-- Favicon and Apple Icons -->
-    <link rel="shortcut icon" href="img/icons/favicon.ico">
-    <link rel="apple-touch-icon" href="img/icons/apple-touch-icon.png">
-    <link rel="apple-touch-icon" href="img/icons/apple-touch-icon-72x72.png" sizes="72x72">
-    <link rel="apple-touch-icon" href="img/icons/apple-touch-icon-114x114.png" sizes="114x114">
+  <!-- Favicon and Apple Icons -->
+  <link rel="shortcut icon" href="img/icons/favicon.ico">
+  <link rel="apple-touch-icon" href="img/icons/apple-touch-icon.png">
+  <link rel="apple-touch-icon" href="img/icons/apple-touch-icon-72x72.png" sizes="72x72">
+  <link rel="apple-touch-icon" href="img/icons/apple-touch-icon-114x114.png" sizes="114x114">
+</head>
 
-  </head>
-  <body>
-    
-    
-    
+<body>
 
-    <!-- jQuery: CDN -->
-    <script src="http://code.jquery.com/jquery.js"></script>
-   
-    <!-- Bootstrap: Local -->
-    <script src="js/min/bootstrap-ck.js"></script>
+  <!-- MAIN CONTENT -->
+      
 
-    <!-- Bootstrap: CDN -->
-    <!-- <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script> -->
 
-    <!-- Custom Scripts -->
-    <script src="js/min/scripts-ck.js"></script>
+  <!-- jQuery: CDN -->
+  <script src="http://code.jquery.com/jquery.js"></script>
+ 
+  <!-- Bootstrap: Local -->
+  <script src="js/min/bootstrap-ck.js"></script>
 
-  </body>
+  <!-- Bootstrap: CDN -->
+  <!-- <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script> -->
+
+  <!-- Custom Scripts -->
+  <script src="js/min/scripts-ck.js"></script>
+
+</body>
 </html>
