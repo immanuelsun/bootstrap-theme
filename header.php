067100ebc405ea401b4821831355c04c47a73909
Immanuel Sun <immanuelsun@newstreamsdesign.com>
2014-07-16 14:43:58 -0700

    Clean up files

---
+++ header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Jumbotron Template for Bootstrap</title>
+
+
+    <?php wp_head(); ?>
+  </head>
+
+  <body <?php body_class(); ?>>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="<?php bloginfo( 'url' ); ?>"><?php bloginfo( 'name' ); ?></a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <?php 
+            $defaults = array(
+              'theme_location'  => '',
+              'menu'            => 'header-menu',
+              'container'       => '',
+              'container_class' => '',
+              'container_id'    => '',
+              'menu_class'      => 'nav navbar-nav',
+              'menu_id'         => '',
+              'echo'            => true,
+              'fallback_cb'     => 'wp_page_menu',
+              'before'          => '',
+              'after'           => '',
+              'link_before'     => '',
+              'link_after'      => '',
+              'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+              'depth'           => 4,
+              'walker'          => ''
+            );
+
+            wp_nav_menu( $defaults );
+          ?>
+                  
+        </div><!--/.navbar-collapse -->
+      </div>
+    </div>
---
+++ index_01.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<!-- [if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!-- [if !IE]> <!--> <html lang="en"> <!--<![endif]-->
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title></title>
+  <meta name="desciption"  content="">
+  <meta name="author"  content="">
+
+  <!-- Mobile Specific Meta -->
+ <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+  <!-- Bootstrap -->
+  <!-- <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">-->
+
+  <!-- Bootstrap: Local -->
+  <link rel="stylesheet" href="css/style.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+    <script src="../../docs-assets/js/html5shiv.js"></script>
+    <script src="../../docs-assets/js/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Favicon and Apple Icons -->
+  <link rel="shortcut icon" href="img/icons/favicon.ico">
+  <link rel="apple-touch-icon" href="img/icons/apple-touch-icon.png">
+  <link rel="apple-touch-icon" href="img/icons/apple-touch-icon-72x72.png" sizes="72x72">
+  <link rel="apple-touch-icon" href="img/icons/apple-touch-icon-114x114.png" sizes="114x114">
+</head>
+
+<body>
+
+  <!-- HEADER -->
+  <header class="l-header t-beveled">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <h1 class="logo"><a href="index.php">New Streams Design</a></h1>
+        </div> <!-- end .col-lg-3 -->
+
+        <div class="col-lg-6">
+          <ul class="align-right l-list-divided">
+            <li id="contacti-info">
+              <p><small>Question?</small> </p>
+              <p>Call 1-800-0000</p>
+            </li> <!-- end #contacti-info -->
+            <li>
+              <button type="button" class="button">Buy Now</button>
+            </li>
+          </ul> <!-- end .align-right l-list-divided -->
+        </div> <!-- end .col-lg-9 -->
+      </div> <!-- end .row -->
+    </div> <!-- end .container -->
+  </header>
+
+  <!-- BANNER -->
+  <section>
+    <div class="t-main-banner t-margin" id="main-banner">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-5">
+            <img src="images/iPhone.png" alt="iPhone" class="l-featured-image">
+          </div> <!-- end .col-lg-5 -->
+          <div class="col-lg-7">
+            <h1>Product Name</h1>
+            <h3>This is awesome product description or tagline.</h3>
+
+            <div class="btn-group">
+              <button type="button" class="btn button">Buy Now</button>
+              <button type="button" class="btn button button-secondary">Try it Free</button>
+            </div> <!-- end .btn-group -->
+
+            <hr>
+            
+            <a href=""><img src="images/video-1.png" alt="Video"></a>
+            <a href=""><img src="images/video-2.png" alt="Video"></a>
+          </div> <!-- end .col-lg-7 -->
+        </div> <!-- end .row -->
+      </div> <!-- end .container -->
+    </div> <!-- end #main-banner.t-main-banner t-margin -->
+  </section>
+
+  <!-- TESTIMONIALS -->
+  <section>
+    <div class="container t-margin">
+      <div class="col-lg-4">
+        <blockquote>
+          <p>“This is the first testimonial. You can add up to three testimonials at a time. You also have the option to manually slide the testimonials.”</p>
+          <cite>- Adi Purdila, <a href="#">Website Inc.</a></cite>
+        </blockquote>
+      </div> <!-- end .col-lg-4 -->
+      <div class="col-lg-4">
+        <blockquote>
+          <p>“This is another great testimonial by a non-biased author on ThemeForest.”</p>
+          <cite>- Adi Purdila, <a href="#">Author of Conversion</a></cite>
+        </blockquote>
+      </div> <!-- end .col-lg-4 -->
+      <div class="col-lg-4">
+        <blockquote>
+          <p>“Let’s add a third testimonial. This page easily supports multiple testimonials which also slide automatically.”</p>
+          <cite>- Adi Purdila, <a href="#">Author of Conversion</a></cite>
+        </blockquote>
+      </div> <!-- end .col-lg-4 -->
+    </div> <!-- end .container t-margin -->
+  </section>
+      
+  <hr >
+
+  <!-- FEATURES -->
+  <section>
+    <div class="container t-margin">
+      <header class="text-center t-margin">
+        <h2>For just $10 you’ll get an awesome product.</h2>
+        <h3>Here’s some of its main features.</h3>
+      </header> <!-- end .text-center -->
+      <ul class="row l-list-unstyled">
+        <li class="col-lg-6">
+          <div class="l-image-well text-center"><img src="images/icons/icon-1.png" alt=" "></div>
+          <div class="l-text-well">
+            <strong>Feature Title</strong>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint animi nemo officia, eveniet repellat sed necessitatibus quod atque nobis iure.</p>
+          </div>
+        </li>
+        <li class="col-lg-6">
+          <div class="l-image-well text-center"><img src="images/icons/icon-2.png" alt=""></div>
+          <div class="l-text-well">
+            <strong>Feature Title</strong>
+            <p>Dolores sapiente corrupti eveniet suscipit, est asperiores quo, quia expedita ea cum libero nulla vero modi laboriosam repudiandae maiores, incidunt.</p>
+          </div> 
+        </li>
+      </ul>
+      <ul class="row l-list-unstyled">
+        <li class="col-lg-6">
+          <div class="l-image-well text-center"><img src="images/icons/icon-3.png" alt=""></div>
+          <div class="l-text-well">
+            <strong>Feature Title</strong>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore saepe explicabo culpa eius voluptatum reprehenderit, dolorem pariatur dolor quisquam, tempore.</p>
+          </div> 
+        </li>
+        <li class="col-lg-6">
+          <div class="l-image-well text-center"><img src="images/icons/icon-4.png" alt=""></div>
+          <div class="l-text-well">
+            <strong>Feature Title</strong>
+            <p>Totam amet laborum iste, similique dolorem eius obcaecati, eaque soluta blanditiis minima exercitationem rerum, aperiam. Illum velit atque aliquam corporis.</p>
+          </div> 
+        </li>
+      </ul>
+      <ul class="row l-list-unstyled">
+        <li class="col-lg-6">
+          <div class="l-image-well text-center"><img src="images/icons/icon-5.png" alt=""></div>
+          <div class="l-text-well">
+            <strong>Feature Title</strong>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem earum, consequatur vero eligendi delectus ad sint, exercitationem et dolorem nulla!</p>
+          </div>
+        </li>
+        <li class="col-lg-6">
+          <div class="l-image-well text-center"><img src="images/icons/icon-6.png" alt=""></div>
+          <div class="l-text-well">
+            <strong>Feature Title</strong>
+            <p>Officia repellendus provident doloribus quia voluptatibus, amet laborum! Accusamus rem, suscipit nobis ab eligendi dolore quos maiores expedita culpa voluptatibus!</p>
+          </div>
+        </li>
+      </ul>
+    </div> <!-- end .container t-margin -->
+  </section>
+
+  <!-- CALL-TO-ACTION -->
+  <section>
+    <div class="container t-margin l-callout t-callout clearfix">
+      <div class="row">
+        <div class="col-lg-8">
+          <h2>Buy now or try it free. It’s super easy to start.</h2>
+          <h3>No credit card required for the free trial.</h3>
+        </div> <!-- end .col-lg-8 -->
+        <div class="col-lg-4 text-right">
+          <div class="btn-group">
+            <button type="button" class="btn button">Buy Now</button>
+            <button type="button" class="btn button button-primary">Try it Free</button>
+          </div> <!-- end .btn-group -->
+        </div> <!-- end .col-lg-5 -->
+      </div> <!-- end .row -->
+    </div> <!-- end .container t-margin l-callout t-callout clearfix -->
+  </section>
+
+  <hr>
+
+  <!-- FOOTER -->
+  <footer class="l-footer container t-margin text-center">
+    <ul class="l-list-inline t-margin">
+      <li><a href="#" class="social-icon"><i class="fa fa-twitter"></i></a></li>
+      <li><a href="#" class="social-icon"><i class="fa fa-facebook"></i></a></li>
+      <li><a href="#" class="social-icon"><i class="fa fa-envelope"></i></a></li>
+    </ul>
+
+    <p class="l-copyright">
+      &copy; Copyright 2014 Product Name
+    </p>
+  </footer>
+
+  <!-- jQuery: CDN -->
+  <script src="http://code.jquery.com/jquery.js"></script>
+ 
+  <!-- Bootstrap: Local -->
+  <script src="js/min/bootstrap-ck.js"></script>
+
+  <!-- Bootstrap: CDN -->
+  <!-- <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script> -->
+
+  <!-- Custom Scripts -->
+  <script src="js/min/scripts-ck.js"></script>
+
+</body>
+</html>
